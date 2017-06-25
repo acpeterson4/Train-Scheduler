91c8e12ee5cdba2832757d3db0ac4df6b1152c77
Andrew Peterson <acpeterson4@gmail.com>
2017-06-24 23:26:49 -0500

    pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+var config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  storageBucket: ""
+};
+
+firebase.initializeApp(config);
+
+// Create a variable to reference the database
+var database = firebase.database();
+
+// Use the initialValue below
+// var initialValue =
